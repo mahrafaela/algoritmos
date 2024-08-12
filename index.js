9c11fe3652ad766a03f69111f7a624106c9e2ee6
mahrafaela <128821670+mahrafaela@users.noreply.github.com>
2024-08-12 09:00:38 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+//                     0  1   2   3   4   5 
 const precosLivros = [25, 15, 30, 50, 45, 20];
 
 let atual = 0;
 let maisBarato = 0;
 
+for(let atual = 0; atual<precosLivros.length; atual ++){
+   if(precosLivros[atual]<precosLivros[maisbarato]){
+    maisBarato = atual
+   }     
+}
+console.log(`O livro mais barato custa ${precosLivros[maisBarato]}`)
 
 
